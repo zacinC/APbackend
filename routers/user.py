9142f8d58120d7374a59c76b9cfd23acff5bba6c
Uroš Milović <urkemilovic16@gmail.com>
2024-07-26 16:11:10 +0200

    added limit and offset to getting users

--- routers/user.py
+++ routers/user.py
@@ -11,16 +11,16 @@ from ..schemas import schemas
 user_router = APIRouter()
 
 
-@user_router.get("/users/", response_model=list[schemas.User],tags = ["user"])
-def get_all_users(email:Optional[str] = None,name:Optional[str] = None,lastname:Optional[str] = None,db: Session = Depends(get_db)):
+@user_router.get("/users/{page_number}", response_model=list[schemas.User],tags = ["user"])
+def get_all_users(page_number:int,email:Optional[str] = None,name:Optional[str] = None,lastname:Optional[str] = None,role:Optional[str] = None,db: Session = Depends(get_db)):
     users = []
 
     print(email,name,lastname)
 
-    if(email or name or lastname):
-        users = get_users_filtered(db,name = name,lastname = lastname,email = email)
+    if(email or name or lastname or role):
+        users = get_users_filtered(page_number,db,name = name,lastname = lastname,email = email,role = role)
     else:
-        users = get_users(db)
+        users = get_users(db,page_number)
     return users
 
 
--- services/user.py
+++ services/user.py
@@ -4,23 +4,28 @@ from ..MySql import models
 from ..schemas import schemas
 
 
-def get_users(db: Session):
-    return db.query(models.User).all()
+def get_users(db: Session,page_number):
+    return db.query(models.User).offset((page_number-1)*10).limit(10).all()
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-def get_users_filtered(db:Session,name:Optional[str] = None,lastname:Optional[str] = None,email:Optional[str] = None):
+def get_users_filtered(page_number:int,db:Session,name:Optional[str] = None,lastname:Optional[str] = None,email:Optional[str] = None,role:Optional[str] = None):
     if not email:
         email = ""
     if not name:
         name = ""
     if not lastname:
         lastname = ""
+    if not role:
+        role = ""
     
     print(email)
 
-    return db.query(models.User).filter(models.User.email.like(f'%{email}%'),models.User.name.like(f'%{name}%'),models.User.lastname.like(f'%{lastname}%'))
+    return db.query(models.User).filter(models.User.email.like(f'%{email}%'),
+                                        models.User.name.like(f'%{name}%'),
+                                        models.User.lastname.like(f'%{lastname}%'),
+                                        models.User.role_type.like(f'%{role}')).offset((page_number-1)*10).limit(10).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
     fake= user.password # ovdje ide logika za hesovanje passworda
