48034fb51704ef0475faa6e6f76ecab25a1aa517
Vedad Selmanovic <dbzvegetassj@outlook.com>
2024-08-04 19:15:36 +0200

    Added registration for the user

--- auth/utils.py
+++ auth/utils.py
@@ -7,7 +7,7 @@ from sib_api_v3_sdk.rest import ApiException
 import logging
 
 from ..schemas.schemas import UserBase
-from ..settings import EMAIL_RESET_TOKEN_EXPIRE_HOURS, SECRET_KEY, BREVO_API_KEY
+from ..settings import DOMAIN, EMAIL_RESET_TOKEN_EXPIRE_MINUTES, SECRET_KEY, BREVO_API_KEY, ENV
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -33,7 +33,7 @@ def verify_password_reset_token(token: str) -> str | None:
 
 
 def generate_password_reset_token(email: str) -> str:
-    delta = timedelta(hours=EMAIL_RESET_TOKEN_EXPIRE_HOURS)
+    delta = timedelta(minutes=EMAIL_RESET_TOKEN_EXPIRE_MINUTES)
     now = datetime.now(timezone.utc)
     expires = now + delta
     exp = expires.timestamp()
@@ -54,32 +54,59 @@ def generate_password_reset_email(user: UserBase):
         "project_name": project_name,
         "username": user.username,
         "email": user.email,
-        "valid_hours": EMAIL_RESET_TOKEN_EXPIRE_HOURS,
-        "link": f'localhost:8000/recover-password/{token}'
+        "valid_hours": EMAIL_RESET_TOKEN_EXPIRE_MINUTES,
+        "link": f'{DOMAIN}/recover-password/{token}' if ENV == 'PRODUCTION' else f'localhost:8000/recover-password/{token}'
     }
 
-    html_content = render_email_template(context=context)
+    html_content = render_email_template("password_reset", context)
 
     return {"subject": subject, "html_content": html_content}
 
 
-def render_email_template(context: dict) -> str:
-    html_content = f"""
-    <html>
-      <body>
-        <p>Poštovani {context["username"]},</p>
-        <p>
-          Zatražili ste resetovanje lozinke na platformi {context["project_name"]}.
-          Kliknite na link ispod za resetovanje lozinke:
-        </p>
-        <p><a href="{context["link"]}">Resetuj Lozinku</a></p>
-        <p>Ovaj link je validan narednih {context["valid_hours"]} sati.</p>
-        <p>Ukoliko niste zatražili resetovanje lozinke, ignorišite ovaj mejl.</p>
-        <p>Srdačno, APCG.</p>
-      </body>
-    </html>
-    """
-    return html_content
+def generate_account_registration_email(user: UserBase):
+    # We use everything like in password reset function with exception to subject and email template
+    project_name = "Autobuski Prevoz Crne Gore"
+    subject = f"{project_name} - Aktivacija naloga za korisnika {user.email}"
+
+    token = generate_password_reset_token(user.username)
+    context = {
+        "project_name": project_name,
+        "username": user.username,
+        "email": user.email,
+        "valid_hours": EMAIL_RESET_TOKEN_EXPIRE_MINUTES,
+        "link": f'{DOMAIN}/verify-account/{token}' if ENV == 'PRODUCTION' else f'localhost:8000/verify-account/{token}'
+    }
+
+    html_content = render_email_template("account_registration", context)
+
+    return {"subject": subject, "html_content": html_content}
+
+
+def render_email_template(template_name: str, context: dict) -> str:
+    template = EMAIL_TEMPLATES.get(template_name, "")
+    if not template:
+        raise ValueError("Invalid template name provided")
+
+    return template.format(**context)
+
+
+# def render_email_template(context: dict) -> str:
+#     html_content = f"""
+#     <html>
+#       <body>
+#         <p>Poštovani {context["username"]},</p>
+#         <p>
+#           Zatražili ste resetovanje lozinke na platformi {context["project_name"]}.
+#           Kliknite na link ispod za resetovanje lozinke:
+#         </p>
+#         <p><a href="{context["link"]}">Resetuj Lozinku</a></p>
+#         <p>Ovaj link je validan narednih {context["valid_hours"]} sati.</p>
+#         <p>Ukoliko niste zatražili resetovanje lozinke, ignorišite ovaj mejl.</p>
+#         <p>Srdačno, APCG.</p>
+#       </body>
+#     </html>
+#     """
+#     return html_content
 
 
 def send_email(user: UserBase, html_content, subject):
@@ -99,5 +126,37 @@ def send_email(user: UserBase, html_content, subject):
         logging.info(f'Mail sent to {user.email}')
 
     except ApiException as e:
-        print(
+        logging.info(
             "Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+
+
+EMAIL_TEMPLATES = {
+    "password_reset": """
+    <html>
+      <body>
+        <p>Poštovani {username},</p>
+        <p>
+          Zatražili ste resetovanje lozinke na platformi {project_name}.
+          Kliknite na link ispod za resetovanje lozinke:
+        </p>
+        <p><a href="{link}">Resetuj Lozinku</a></p>
+        <p>Ovaj link je validan narednih {valid_hours} minuta.</p>
+        <p>Ukoliko niste zatražili resetovanje lozinke, ignorišite ovaj mejl.</p>
+        <p>Srdačno, APCG.</p>
+      </body>
+    </html>
+    """,
+    "account_registration": """
+    <html>
+      <body>
+        <p>Poštovani {username},</p>
+        <p>
+          Dobrodošli na {project_name}. Kliknite na link ispod da potvrdite vašu email adresu i aktivirate vaš nalog:
+        </p>
+        <p><a href="{link}">Aktiviraj Nalog</a></p>
+        <p>Ovaj link je validan narednih {valid_hours} minuta.</p>
+        <p>Srdačno, APCG.</p>
+      </body>
+    </html>
+    """
+}
--- routers/login.py
+++ routers/login.py
@@ -96,6 +96,27 @@ async def password_recovery_form(request: Request, token: str):
     return templates.TemplateResponse("reset_password.html", {"request": request, "token": token})
 
 
+@login_router.get("/verify-account/{token}", response_class=HTMLResponse, tags=['login'])
+async def account_verification(request: Request, token: str, db: Session = Depends(get_db)):
+    username = verify_password_reset_token(token=token)
+    if not username:
+        raise HTTPException(status_code=400, detail="Invalid token")
+
+    user = get_user_by_username(db, username)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this email does not exist in the system.",
+        )
+
+    user.disabled = False
+    db.add(user)
+    db.commit()
+
+    message = "Nalog je uspješno aktiviran!"
+    return templates.TemplateResponse("successfull_verification.html", {"request": request, "message": message})
+
+
 @login_router.post("/recover-password", response_model=Message, tags=['login'])
 async def recover_password(email: str, db: Session = Depends(get_db)):
     user = get_user_by_email(db, email)
