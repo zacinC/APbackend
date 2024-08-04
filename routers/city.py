bf6092f8454129dcc9aa75b3f1f93b86df2b0441
Uroš Milović <urkemilovic16@gmail.com>
2024-08-04 18:52:41 +0200

    added insert for cities and countries

--- routers/city.py
+++ routers/city.py
@@ -5,7 +5,7 @@ from fastapi import Depends
 from fastapi import APIRouter, status
 
 from ..database.dbconfig import get_db
-from ..services.city import get_cities
+from ..services.city import get_cities, insert_city
 from ..schemas import schemas
 
 city_router = APIRouter()
@@ -15,3 +15,7 @@ city_router = APIRouter()
 @city_router.get("/cities", response_model=List[schemas.CityCreate], tags=["city"])
 def get_all_cities(city_name:Optional[str] = None,db: Session = Depends(get_db)):
     return get_cities(db = db,city_name=city_name)
+
+@city_router.post("/cities", response_model=List[schemas.CityCreate], tags=["city"])
+def post_city(city:schemas.CityCreate,db:Session = Depends(get_db)):
+    return insert_city(city,db)
--- routers/country.py
+++ routers/country.py
@@ -5,7 +5,7 @@ from fastapi import Depends
 from fastapi import APIRouter, status
 
 from ..database.dbconfig import get_db
-from ..services.country import get_countries
+from ..services.country import get_countries, insert_country
 from ..schemas import schemas
 
 country_router = APIRouter()
@@ -15,3 +15,7 @@ country_router = APIRouter()
 @country_router.get("/countries", response_model=List[schemas.CountryCreate], tags=["country"])
 def get_all_countries(db: Session = Depends(get_db)):
     return get_countries(db)
+
+@country_router.post("/cities", response_model=List[schemas.CityCreate], tags=["city"])
+def post_country(country:schemas.CountryCreate,db:Session = Depends(get_db)):
+    return insert_country(country,db)
