9f298dfd66ce62daf501bcca05c2fc00c9342f64
Vedad Selmanovic <dbzvegetassj@outlook.com>
2024-08-05 21:01:03 +0200

    some fixes and files needed for deployment

--- routers/station.py
+++ routers/station.py
@@ -4,24 +4,23 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from fastapi import APIRouter, status
 
-from AutobuskeBackend.auth.deps import get_current_admin_user
+from ..auth.deps import get_current_admin_user
 
 from ..database.dbconfig import get_db
-from ..services.station import get_stations, add_station, update_station, get_stations_filtered, delete_station_id,get_stations_count
+from ..services.station import get_stations, add_station, update_station, get_stations_filtered, delete_station_id, get_stations_count
 from ..schemas import schemas
 
 station_router = APIRouter()
 
+
 @station_router.get("/stations/count", response_model=int, tags=["station"])
 def get_all_stations_count(db: Session = Depends(get_db)):
-    return get_stations_count(db = db)
+    return get_stations_count(db=db)
+
 
 @station_router.get("/stations/{page_number}", response_model=List[schemas.Station], tags=["station"])
-def get_all_stations(page_number:int,db: Session = Depends(get_db)):
-    return get_stations(page_number = page_number,db = db)
-
-
-
+def get_all_stations(page_number: int, db: Session = Depends(get_db)):
+    return get_stations(page_number=page_number, db=db)
 
 
 @station_router.get("/stations-filtered", response_model=List[schemas.Station], tags=["station"])
@@ -30,15 +29,15 @@ def get_all_stations_filtered(search: str, db: Session = Depends(get_db)):
 
 
 @station_router.post("/stations", response_model=schemas.StationCreate, tags=["station"])
-def post_station(current_user: Annotated[schemas.User, Depends(get_current_admin_user)],station: schemas.StationCreate, db: Session = Depends(get_db)):
+def post_station(current_user: Annotated[schemas.User, Depends(get_current_admin_user)], station: schemas.StationCreate, db: Session = Depends(get_db)):
     return add_station(station, db)
 
 
-@station_router.put("/stations/{id}",response_model=schemas.Station, tags=["station"], status_code=status.HTTP_201_CREATED)
-def put_station(current_user: Annotated[schemas.User, Depends(get_current_admin_user)],id, station: schemas.StationCreate, db: Session = Depends(get_db)):
+@station_router.put("/stations/{id}", response_model=schemas.Station, tags=["station"], status_code=status.HTTP_201_CREATED)
+def put_station(current_user: Annotated[schemas.User, Depends(get_current_admin_user)], id, station: schemas.StationCreate, db: Session = Depends(get_db)):
     return update_station(station, id, db)
 
 
 @station_router.delete("/stations/{id}", tags=["station"])
-def delete_station(current_user: Annotated[schemas.User, Depends(get_current_admin_user)],id: int, db: Session = Depends(get_db)):
+def delete_station(current_user: Annotated[schemas.User, Depends(get_current_admin_user)], id: int, db: Session = Depends(get_db)):
     return delete_station_id(id, db)
