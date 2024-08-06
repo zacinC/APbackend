3a914fa17c947e365fa9d0edda9e3e080e1b4d83
Vedad Selmanovic <dbzvegetassj@outlook.com>
2024-08-06 13:55:22 +0200

    CORS changes

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "https://apcg-nine.vercel.app/",
+    "https://apcg-nine.vercel.app",
     "https://apcg-ex5hk1hrz-vedads-projects.vercel.app",
-    "apcg.vercel.app",
-    "apcg-jovan02s-projects.vercel.app",
-    "apcg-23ummtbyj-jovan02s-projects.vercel.app"
+    "https://apcg.vercel.app",
+    "https://apcg-jovan02s-projects.vercel.app",
+    "https://apcg-23ummtbyj-jovan02s-projects.vercel.app"
 
 ]
 
