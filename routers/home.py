51b187928c5be34f4cacc334c831af85955fd346
Vedad Selmanovic <dbzvegetassj@outlook.com>
2024-08-09 18:39:12 +0200

    changes om home route

--- routers/home.py
+++ routers/home.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 home_router = APIRouter()
 
 
-@home_router.get('/', include_in_schema=False)
+@home_router.get('/home', include_in_schema=False)
 def get_home():
     return "Dobrodošli na API autobuskog prevoza Crne Gore"
